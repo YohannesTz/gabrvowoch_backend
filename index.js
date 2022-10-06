9773559b8f3baf992ee27fe6d51662865cb459b8
yohannes <yohannes222ethiopia@gmail.com>
2022-10-06 21:50:32 +0300

    increased request limit

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const app = express();
 const mongoString = process.env.DATABASE_URL
 
 app.use(cors())
+app.use(express.json({ limit: "1mb" }));
 app.use(express.json());
 
 mongoose.connect(mongoString);
