13615fe5a5eb3ffd5ce55f997a8a0c0bd12f22fb
yohannes <yohannes222ethiopia@gmail.com>
2022-10-06 19:07:24 +0300

    added dynamic port

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { Joke } = require('./models/joke');
+const port = process.env.PORT || 5000;
+
 require('dotenv').config();
 
 const app = express();
