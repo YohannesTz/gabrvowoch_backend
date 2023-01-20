63fc999b1af339dd71752a2f8ba1cd99267acf6e
besufikad17 <besufikadtialhun1994@gmail.com>
2023-01-20 22:15:23 +0300

    joke controller added

--- index.js
+++ index.js
@@ -1,53 +1,32 @@
 const express = require('express');
 const cors = require('cors');
-
-const mongoose = require('mongoose');
-const { Joke } = require('./models/joke');
-const port = process.env.PORT || 5000;
-
+const routes = require('./routes/routes');
+const { PrismaClient } = require('@prisma/client');
 require('dotenv').config();
 
+const port = process.env.PORT || 5000;
 const app = express();
-const mongoString = process.env.DATABASE_URL
+const prisma = new PrismaClient();
+
+
+async function connectDB(){
+    try {
+        await prisma.$connect();
+        console.log('? Database connected successfully');
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    } finally {
+        await prisma.$disconnect();
+    }
+}
+
+connectDB();
 
 app.use(cors())
-app.use(express.json({ limit: "1mb" }));
 app.use(express.json());
+app.use("/", routes);
 
-mongoose.connect(mongoString);
-const database = mongoose.connection;
-
-
-database.on('error', (error) => {
-    console.log(error)
-})
-
-database.once('connected', () => {
-    console.log('Database Connected');
-})
-
-app.get("/", async(req,res) => {
-    return res.status(200).json("Server is running!");
-});
-
-app.get("/getJokes", async(req,res) => {
-    return res.status(200).json("get jokes works!");
-});
-
-app.get("/jokes", async(req, res) => {
-    const allJokes = await Joke.find();
-    return res.status(200).json(allJokes);
-});
-
-app.get("/jokess", async(req,res) => {
-    
-})
-
-app.post("/jokes", async(req, res) => {
-    const newJoke = new Joke({...req.body});
-    const insertedJoke = await newJoke.save();
-    return res.status(201).json(insertedJoke);
-})
 
 app.listen(port, () => {
     console.log(`Server Started at ${port}`)
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,12 @@
 const route = require('express').Router();
+const jokeController = require('../controllers/jokeController');
 
+route.post("/add", jokeController.addJoke);
 
-route.get("/all", )
+route.get("/all", jokeController.getAllJokes);
+
+route.get("/random", jokeController.getRandomJoke);
+
+route.get("/joke/:id", jokeController.getJokeById);
 
  module.exports = route;
